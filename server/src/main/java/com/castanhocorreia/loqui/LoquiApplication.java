cbfbc479be63194181fdb8ed9eed0a292a9352fb
Castanho Correia <me@castanhocorreia.com>
2022-02-19 16:11:58 -0300

    Generate spring bootstrap files

---
+++ server/src/main/java/com/castanhocorreia/loqui/LoquiApplication.java
@@ -0,0 +1,13 @@
+package com.castanhocorreia.loqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoquiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/castanhocorreia/loqui/LoquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castanhocorreia.loqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
